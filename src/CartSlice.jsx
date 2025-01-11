c38c5cca429d824542bf37b22a20ae681e9d4f1e
Rohit Ravikumar <rohithuma123@gmil.com>
2025-01-10 19:39:53 -0500

    Finished project

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,38 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numItems: 0,
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if(existingItem) {
+            existingItem.quantity++;
+        }
+        else {
+            state.items.push({name,image,cost,quantity: 1});
+        }
+        state.numItems += 1;
     },
     removeItem: (state, action) => {
+     const { name, quantity } = action.payload;
+       state.items = state.items.filter(item => item.name !== name);
+       state.numItems -= quantity;
+       if (state.numItems < 0) {
+        state.numItems = 0;
+    }
+       
     },
     updateQuantity: (state, action) => {
+        const {name, quantity } = action.payload;
+        const item = state.items.find(item => item.name === name);
+        if(item) {
+            const differenceQuantity = quantity - item.quantity; 
+            state.numItems += differenceQuantity;
+            item.quantity = quantity;
 
-    
+        }
     },
   },
 });
